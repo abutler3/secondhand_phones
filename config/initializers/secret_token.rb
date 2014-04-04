9ea49255f134ab78e96971c2af59f6b3596b3524
Drew Butler <abutler3@gmail.com>
2014-04-03 20:24:20 -0500

    Add bootstrap and listings model

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PhoneSales::Application.config.secret_key_base = '605dd650e7fc4160e1e0567983b0facf6469c4fb87433a125a7ee01e969cbe88b8418c2deafbb67fbba5b184bf8ae10fa327aad8acd3bacfccb500af4e06b63a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PhoneSales::Application.config.session_store :cookie_store, key: '_phone_sales_session'
